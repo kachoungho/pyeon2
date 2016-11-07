3355f805983292f8137293d72772082418fa81ea
silverjpk <silverjpk123@naver.com>
2016-11-07 16:18:21 +0900

    VO 잡기~

---
+++ src/main/java/com/pyeon2/vo/ItemVO.java
@@ -0,0 +1,63 @@
+package com.pyeon2.vo;
+
+public class ItemVO {
+
+	private String item_code;
+	private String item_name;
+	private String item_image;
+	private int price;
+	private int count;
+	private int hit;
+	private String category;
+	private String area;
+	
+	public String getItem_code() {
+		return item_code;
+	}
+	public void setItem_code(String item_code) {
+		this.item_code = item_code;
+	}
+	public String getItem_name() {
+		return item_name;
+	}
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+	public String getItem_image() {
+		return item_image;
+	}
+	public void setItem_image(String item_image) {
+		this.item_image = item_image;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+}
---
+++ src/main/java/com/pyeon2/vo/MemberVO.java
@@ -0,0 +1,70 @@
+package com.pyeon2.vo;
+
+public class MemberVO {
+	
+	private String id;
+	private String pwd;
+	private String name;
+	private String position;
+	private String phone;
+	private String age;
+	private String address;
+	private String gender;
+	private String area;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+}
