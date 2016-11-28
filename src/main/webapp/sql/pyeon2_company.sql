76db8715270c0f3fd315e44a795cdba1e3e6cb20
kachoungho <kachoungho@naver.com>
2016-11-28 18:45:11 +0900

    풀 받고 푸시

--- src/main/webapp/sql/pyeon2_company.sql
+++ src/main/webapp/sql/pyeon2_company.sql
@@ -169,7 +169,7 @@ insert into company_item(bno, item_code,item_name,item_image,cost,price,count,ca
 insert into company_item(bno, item_code,item_name,item_image,cost,price,count,category ) values(company_item_num.nextVal, 'ALCOHOLIC108','CASS FRESH 500ml 캔',
 'http://callmart-gimpo.com/data/item/1440838986/thumb-7Lm07Iqk500ml_300x300.jpg',1300,2600,500,'주류' );
 
-
+select * from company_item;
 alter table company_item add code1 varchar2(20);
 alter table company_item add code2 number(10);
 update company_item set code1 = 'DRINK' , code2 = 110 where item_code = 'DRINK110';
